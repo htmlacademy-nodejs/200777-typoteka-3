9209502c3ac3a721d70f51fe31fc25e0b96cd4d0
Leks <leks511@bk.ru>
2020-11-27 21:50:57 +0300

    Добавит рендеринг шаблонов по get-запросам

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/articles/:id`, (req, res) => res.render(`post`));
-articlesRouter.get(`/articles/add`, (req, res) => res.render(`new-post`));
-articlesRouter.get(`/articles/category/:id`, (req, res) => res.render(`articles-by-category`));
-articlesRouter.get(`/articles/edit/:id`, (req, res) => res.render(`post`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
+articlesRouter.get(`/add`, (req, res) => res.render(`articles/new-post`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/post`));
 
 module.exports = articlesRouter;
