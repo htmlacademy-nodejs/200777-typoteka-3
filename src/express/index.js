a1e2ab1e1f69047b850d1703d15b4b55e4aa79ac
Leks <leks511@bk.ru>
2020-11-27 21:50:57 +0300

    Поправит пути для css, fonts, img. Добавит директории для шаблонов, поправит шаблоны

--- src/express/index.js
+++ src/express/index.js
@@ -12,16 +12,19 @@ const PUBLIC_DIR = `public`;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
+
 app.use((req, res) => res.status(404).render(`errors/404`));
-app.use((err, req, res) => res.status(404).render(`errors/500`));
+app.use((err, req, res, _next) => res.status(404).render(`errors/500`));
 
 app.listen(DEFAULT_PORT, () => console.log(`Сервер работает на ${DEFAULT_PORT}`));
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/my`, (req, res) => res.render(`my`));
-myRouter.get(`/my/comments`, (req, res) => res.render(`comments`));
+myRouter.get(`/`, (req, res) => res.render(`my/my`));
+myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
 
 module.exports = myRouter;
